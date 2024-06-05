dd2a504f16fcd21fa552917b86a70e76efb40afa
Amarjeet Kumar <bidupur8544@gmail.com>
2024-06-05 15:39:09 +0530

    login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,10 +7,12 @@ import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { SideBarComponent } from './home/side-bar/side-bar.component';
+import { LoginPageComponent } from './login-page/login-page.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    LoginPageComponent,
     SideBarComponent
   ],
   imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
