bf6ba92de682243b24a316cc93e99049f00b3822
Amarjeet Kumar <bidupur8544@gmail.com>
2024-07-06 16:43:20 +0530

    final

--- src/app/home/admin-chat-list/admin-chat-list.component.ts
+++ src/app/home/admin-chat-list/admin-chat-list.component.ts
@@ -1,30 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { NavigationEnd, Router } from '@angular/router';
 import { CurdService } from 'src/app/service/curd.service';
 import { SharedService } from 'src/app/service/shared.service';
+import { filter } from 'rxjs/operators';
 
 @Component({
   selector: 'app-admin-chat-list',
   templateUrl: './admin-chat-list.component.html',
   styleUrls: ['./admin-chat-list.component.scss'],
 })
-export class AdminChatListComponent  implements OnInit {
+export class AdminChatListComponent implements OnInit {
   chat_mes_list: any;
   img_url: any;
 
   constructor(
-    private _crud:CurdService,
-    private _shared:SharedService,
-    private _router:Router
-  ) { 
+    private _crud: CurdService,
+    private _shared: SharedService,
+    private _router: Router
+  ) {
     this._shared.img_base_url.subscribe(
-      (res:any)=>{
-        this.img_url=res
+      (res: any) => {
+        this.img_url = res
       }
     )
   }
 
   ngOnInit() {
+    this._router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe(() => {
+      this.loadData();
+    });
+
+    this.loadData();
+  }
+  loadData() {
     this._crud.get_admin_chat_any().subscribe(
       (res: any) => {
         this.chat_mes_list = res.Data
@@ -34,7 +42,7 @@ export class AdminChatListComponent  implements OnInit {
       }
     );
   }
-  ondetailschat(data:any){
+  ondetailschat(data: any) {
     this._shared.shared_details.next(data)
     this._router.navigate(['/home/helpdesk'])
   }
